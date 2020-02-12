c526c22ca85954d8b59f1ed61f90af09b4a0c7a3
Igor Theodoro <igortheodoro12@gmail.com>
2020-02-12 15:46:17 -0300

    Adição do envio de e-mail automático

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-var express = require('express'),
-  app = express();
+const express = require('express'),
+  app = express(),
+  mailgun = require("mailgun-js"),
+  bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+const DOMAIN = "sandbox5a7ce9437dd748c383f7d184c9a274cb.mailgun.org";
+const mg = mailgun({apiKey: "3ff3141a88e0bb92173989e6c87626e3-52b6835e-a79e8689", domain: DOMAIN});
 
 app.set('view engine', 'ejs')
 app.use(express.static('public')); //Usará a pasta public
@@ -9,15 +16,27 @@ app.get('/', function (req, res) {
   res.render('home');
 });
 
-app.get('/jobs', function (req, res) {
-  res.render('jobs');
-});
+app.post('/', function (req, res) { 
+  const data = {
+    from: req.body.email,
+    to: "igortheodoro12@gmail.com",
+    subject: req.body.name,
+    text: req.body.content
+  };
+  mg.messages().send(data, function (error, body) {
+    console.log(body);
+  });
+
+  res.redirect('/')
+})
+
+
 
 app.get('*', function (req, res) {
   //Se não existir a URL pesquisada, redirecione para a rota raiz
   res.redirect('/');
 });
 
-app.listen(process.env.PORT || 8080, function () {
+app.listen(3000, function () {
   console.log('Running')
 })
